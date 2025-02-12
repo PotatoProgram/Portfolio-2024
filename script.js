02553e0e2cbd851fd402df1ff2369d5a11d0b3aa
PotatoProgram <willieb976@gmail.com>
2025-02-12 18:16:52 -0500

    added comments and `answers` definition

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function saveInputs() {
-    event.preventDefault();
-    var answers = (document.getElementById("message-send")).serializeArray;
-    console.log(answers);
+  event.preventDefault();
+  var answers = {
+    //name input (inconsistency due to "name"'s reserved status)
+    nameResponse: document.forms["message-send"]["name"],
+    //email input
+    email: document.forms["message-send"]["email"],
+    //age input
+    age: document.forms["message-send"]["age"],
+    //message input
+    message: document.forms["message-send"]["message"],
+  };
+  //set localstorage item to new inputs
+  localStorage.setItem("loginDetails", JSON.stringify(answers));
+  
 }
