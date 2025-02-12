b87b73c0195ef96377eac380729a211fdd70f3e5
PotatoProgram <willieb976@gmail.com>
2025-02-12 18:53:30 -0500

    finished form handler

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 function saveInputs() {
-  event.preventDefault();
+  //allegedly, event is deprecated
+  //it still works though, and I'm too lazy to find what the alternative is
+    event.preventDefault();
   var answers = {
     //name input (inconsistency due to "name"'s reserved status)
-    nameResponse: document.forms["message-send"]["name"],
+    nameResponse: document.forms["message-send"]["name"].value,
     //email input
-    email: document.forms["message-send"]["email"],
+    email: document.forms["message-send"]["email"].value,
     //age input
-    age: document.forms["message-send"]["age"],
+    age: document.forms["message-send"]["age"].value,
     //message input
-    message: document.forms["message-send"]["message"],
+    message: document.forms["message-send"]["message"].value,
   };
   //set localstorage item to new inputs
   localStorage.setItem("loginDetails", JSON.stringify(answers));
-  
+
+  document.body.append("Login saved with message: " + answers.message);
 }
