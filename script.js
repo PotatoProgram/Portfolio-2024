a4489d5114fc0f2d3c46f8fc67e0a75f4337aff1
PotatoProgram <willieb976@gmail.com>
2025-02-12 19:12:38 -0500

    added script to all pages and finished content replacement function

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+//If localStorage has already been set on page load, use it to replace the "guestProtocol" and "Welcome..." lines
+//otherwise, do nothing
+if (localStorage.loginDetails) {
+    var loginDetails = JSON.parse(localStorage.loginDetails);
+    var welcome = document.getElementById("welcome");
+    var entry = document.getElementById("entry");
+    welcome.innerHTML = "Welcome back, " + loginDetails.nameResponse + ", to the Repository";
+    entry.innerHTML = "C:\\terminal\\" + loginDetails.email + "\\>";
+}
+
+
+
 function saveInputs() {
   //allegedly, event is deprecated
   //it still works though, and I'm too lazy to find what the alternative is
